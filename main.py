9e8e4ec969155e8070b50d30768f4440ed85719d
Murilo Sorbo <developermurilo@gmail.com>
2023-05-09 20:10:29 -0300

    Changed file names and make it call

--- main.py
+++ main.py
@@ -1,51 +1,20 @@
-import pyautogui
-import cv2
-from cvzone.FaceMeshModule import FaceMeshDetector
+import tkinter as tk
+import program
 
-nose = 5
-mouthUp = 13
-mouthDown = 14
+window = tk.Tk()
+window.geometry("400x180")
 
-lastX = 0
-lastY = 0
+text = tk.Label(window, text='Controle de Mouse com Rosto. Selecione uma das opções abaixo')
+btnRun = tk.Button(window, text="Iniciar Programa")
+btnCalibration = tk.Button(window, text="Calibrar")
 
-cap = cv2.VideoCapture(0)
-detector = FaceMeshDetector(maxFaces=1)
+window.title('FabLab Sesi Sorocaba - Controle Mouse')
+text.config(pady=20)
+btnRun.config(width=40, height=2, command=program.Run)
+btnCalibration.config(width=40, height=2)
 
-def calculateToScreen(point, minX, maxX, minY, maxY):
-    width = 1366
-    height = 768
+text.pack()
+btnRun.pack()
+btnCalibration.pack()
 
-    return (width / (maxX - minX)) * (point[0] - minX), (height / (maxY - minY)) * (point[1] - minY)
-
-def clicked(mouthUp, mouthDown):
-    return abs(mouthUp[1] - mouthDown[1]) > 5
-
-while True:
- 
-    ret, img = cap.read()
-
-    if not ret:
-        break
-    
-    img = cv2.flip(img, 1)
-    
-    img, faces = detector.findFaceMesh(img, draw=False)
-
-    if faces:
-        face = faces[0]
-
-        x, y = calculateToScreen(face[nose], 100, 500, 200, 350)
-        click = clicked(face[mouthUp], face[mouthDown])
-
-        if (abs(lastX - x) > 10 or abs(lastY - y) > 10):
-            pyautogui.moveTo(x, y, _pause=False)
-
-            lastX = x
-            lastY = y
-
-        if click:
-            pyautogui.leftClick()
-
-    # cv2.imshow('img', img)
-    # cv2.waitKey(1)
+window.mainloop()
